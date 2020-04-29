30115783b8d70ade792e54a559eb0f23a06ee126
yashprakash13 <yashprakash13@gmail.com>
2020-04-29 11:09:44 +0530

    revamp of new word done

--- app/src/main/java/save/newwords/vocab/remember/ui/OptionsFragment.kt
+++ app/src/main/java/save/newwords/vocab/remember/ui/OptionsFragment.kt
@@ -17,7 +17,6 @@ class OptionsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_options, container, false)
     }
 
